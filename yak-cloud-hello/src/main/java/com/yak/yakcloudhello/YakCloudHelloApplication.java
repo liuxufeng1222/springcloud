9ebadc0bb7eed2d1fa8986ffe2f9b74a43c4b1df
liuxufeng <steven_lxf@163.com>
2020-04-03 14:50:07 +0800

    eureka 代码

---
+++ yak-cloud-hello/src/main/java/com/yak/yakcloudhello/YakCloudHelloApplication.java
@@ -0,0 +1,15 @@
+package com.yak.yakcloudhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class YakCloudHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakCloudHelloApplication.class, args);
+    }
+
+}
---
+++ yak-cloud-hello/src/test/java/com/yak/yakcloudhello/YakCloudHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yak.yakcloudhello;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakCloudHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
