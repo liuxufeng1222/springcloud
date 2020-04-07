6376dd9958315cbbe6fed7e83855e40af99fbb83
liuxufeng <steven_lxf@163.com>
2020-04-07 15:01:21 +0800

    ribbon 加  Hystrix  代码

--- yak-ribbon/src/main/java/com/yak/yakribbon/service/HelloServiceImpl.java
+++ yak-ribbon/src/main/java/com/yak/yakribbon/service/HelloServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yak.yakribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,13 @@ public class HelloServiceImpl {
     @Autowired
     RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod = "helloError")
     public String helloService(String name){
         return restTemplate.getForObject("http://yak-cloud-client/hello?name="+name,String.class);
+    }
+
+    public String helloError(String name){
+        return "hello"+name+"Error!";
     }
 }
