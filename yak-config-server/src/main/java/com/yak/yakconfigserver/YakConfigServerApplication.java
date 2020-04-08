e73c3b60db67fda12a5cfed5e395aa259ba68737
liuxufeng <steven_lxf@163.com>
2020-04-08 11:42:50 +0800

    config server 代码

---
+++ yak-config-server/src/main/java/com/yak/yakconfigserver/YakConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.yak.yakconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class YakConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakConfigServerApplication.class, args);
+    }
+
+}
---
+++ yak-config-server/src/test/java/com/yak/yakconfigserver/YakConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yak.yakconfigserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
